f35b6eaa46e9c4a65f210b8d09b13b7c34facb15
Janet Karpenske <janetkarpenske@gmail.com>
2021-03-01 19:15:24 -0800

    Add forms to sign in and egister to sign in page

--- src/components/LoginRegister.js
+++ src/components/LoginRegister.js
@@ -7,6 +7,26 @@ function LoginRegister (props) {
       <div className="center-align">
         <h1>Login/Register</h1>
         <h2>Coming Soon!</h2>
+        <div className="row">
+        <div className="col-md-6">
+          <div className="card">
+            <h2>Login</h2>
+            <form>
+              <input className="form-control" type="text" placeholder="Email"></input>
+              <input className="form-control" type="text" placeholder="Password"></input>
+            </form>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="card">
+            <h2>Register</h2>
+            <form>
+              <input className="form-control" type="text" placeholder="Email"></input>
+              <input className="form-control" type="text" placeholder="Password"></input>
+            </form>
+          </div>
+        </div>
+        </div>
       </div>
       
     </React.Fragment>
